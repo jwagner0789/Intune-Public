c6addc97e329445aec7e41e78c3bb79c583dcff3
jwagner0789 <78943457+jwagner0789@users.noreply.github.com>
2023-10-17 15:04:29 -0400

    Update ToastScript.cmd

--- Win32Apps/AzurePIMToastActivation/Contents/ToastScript.cmd
+++ Win32Apps/AzurePIMToastActivation/Contents/ToastScript.cmd
@@ -1,1 +1,1 @@
-powershell.exe -WindowStyle hidden -NonInteractive -NoLogo -NoProfile -Command "& '%1'.Replace('pwrshll://', '').Trim('/') '%2'.Trim('/') '%3'.Trim('/') '%4'.Trim('/') '%5'"
+powershell.exe -Version 5.1 -WindowStyle hidden -NonInteractive -NoLogo -NoProfile -Command "& '%1'.Replace('pwrshll://', '').Trim('/') '%2'.Trim('/') '%3'.Trim('/') '%4'.Trim('/') '%5' '%6'.Trim('/') '%7'.Trim('/')"
